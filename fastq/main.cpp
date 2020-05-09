a2b446d40d4174f97e53f03dcd39d325148576ca
Min- <min_z@outlook.com>
2020-05-09 12:10:22 +0800

    Initial Commit

---
+++ fastq/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fastq
+//
+//  Created by Min Zhang on 2020/5/9.
+//  Copyright © 2020 Min Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
